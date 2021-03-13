2b5c7a18df0e2500218634fbe709893c1fffbe4c
Faisal Hussain <faisal@base29.com>
2021-03-13 15:58:10 +0500

    Added relations for ChatRoom and ChatMessage and create a controller

--- app/Models/ChatRoom.php
+++ app/Models/ChatRoom.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\ChatMessage;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ChatRoom extends Model
 {
     use HasFactory;
+
+    public function messages()
+    {
+        return $this->hasMany(ChatMessage::class);
+    }
 }
